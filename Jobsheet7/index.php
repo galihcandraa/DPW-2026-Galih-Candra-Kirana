703288aa3670939658f12bfcc1a425595608f475
galihcandraa <galihcandrakirana17@gmail.com>
2026-09-22 23:28:00 +0700

    7.1 menambahkan includes header & footer

---
+++ Jobsheet7/index.php
@@ -0,0 +1,34 @@
+<?php
+$page_title = "Beranda";
+include __DIR__ . '/includes/header.php';
+?>
+
+<section>
+    <h2>Selamat datang di Sistem Informasi Perpustakaan</h2>
+    <p>Aplikasi digital yang digunakan untuk mengelola seluruh kegiatan operasional perpustakaan secara otomatis.</p>
+</section>
+
+<section>
+    <h2>Ringkasan</h2>
+    <article>
+        <h3>Total Buku</h3>
+        <p>105</p>
+    </article>
+
+    <article>
+        <h3>Total Anggota</h3>
+        <p>15</p>
+    </article>
+
+    <article>
+        <h3>Sedang Dipinjam</h3>
+        <p>2</p>
+    </article>
+
+    <article>
+        <h3>Buku Terlambat</h3>
+        <p>0</p>
+    </article>
+</section>
+
+<?php include __DIR__ . '/includes/footer.php'; ?>
